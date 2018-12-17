e55327c0daa80c8d76edbd274ee4ace267ab55dc
Orbit419 <rkrylov97@gmail.com>
2018-12-17 17:25:41 +0200

    HomeWork Json Update

--- src/main/java/mate/academy/HomeWork/Lesson_8/TradeShop.java
+++ src/main/java/mate/academy/HomeWork/Lesson_8/TradeShop.java
@@ -11,6 +11,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TradeShop {
     private List<Fruit> fruitList = new ArrayList<>();
@@ -44,57 +45,46 @@ public class TradeShop {
     }
 
     public void load(String pathToJsonFile) {
-        fruitList.clear();
+        if (new File(pathToJsonFile).length() != 0) {
+            fruitList.clear();
 
-        try {
-            fruitList = mapper.readValue(new File(pathToJsonFile), new TypeReference<List<Fruit>>() {});
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                fruitList = mapper.readValue(new File(pathToJsonFile), new TypeReference<List<Fruit>>() {});
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        else System.out.println("File is empty!");
     }
 
     public List<Fruit> getSpoiledFruits(Date date) {
-        List<Fruit> spoiledFruits = new ArrayList<>();
-        for (Fruit elem : fruitList) {
-            if (((date.getTime() - elem.getDate().getTime()) / (24 * 60 * 60 * 1000)) > elem.getShelfLife())
-                spoiledFruits.add(elem);
-        }
-
-        return spoiledFruits;
+        return fruitList.stream()
+                .filter(fruit -> dateDifference(date, fruit) > fruit.getShelfLife())
+                .collect(Collectors.toList());
     }
 
     public List<Fruit> getSpoiledFruits(Date date, FruitType type) {
-        List<Fruit> spoiledFruits = new ArrayList<>();
-        for (Fruit elem : fruitList) {
-            if (elem.getType() == type)
-                if (((date.getTime() - elem.getDate().getTime()) / (24 * 60 * 60 * 1000)) > elem.getShelfLife())
-                    spoiledFruits.add(elem);
-        }
-
-        return spoiledFruits;
+        return fruitList.stream()
+                .filter(fruit -> fruit.getType() == type)
+                .filter(fruit -> dateDifference(date, fruit) > fruit.getShelfLife())
+                .collect(Collectors.toList());
     }
 
     public List<Fruit> getAvailableFruits(Date date) {
-        List<Fruit> availableFruits = new ArrayList<>();
-        for (Fruit elem : fruitList) {
-            if (((date.getTime() - elem.getDate().getTime()) / (24 * 60 * 60 * 1000)) < elem.getShelfLife())
-                availableFruits.add(elem);
-        }
-        return availableFruits;
+        return fruitList.stream()
+                .filter(fruit -> dateDifference(date, fruit) < fruit.getShelfLife())
+                .collect(Collectors.toList());
     }
 
     public List<Fruit> getAvailableFruits(Date date, FruitType type) {
-        List<Fruit> availableFruits = new ArrayList<>();
-        for (Fruit elem : fruitList) {
-            if (elem.getType() == type)
-                if (((date.getTime() - elem.getDate().getTime()) / (24 * 60 * 60 * 1000)) < elem.getShelfLife())
-                    availableFruits.add(elem);
-        }
-        return availableFruits;
+        return fruitList.stream()
+                .filter(fruit -> fruit.getType() == type)
+                .filter(fruit -> dateDifference(date, fruit) < fruit.getShelfLife())
+                .collect(Collectors.toList());
     }
 
     public List<Fruit> getAddedFruits(Date date) {
@@ -113,7 +103,12 @@ public class TradeShop {
             if (elem.getType() == type)
                 if (elem.getDate().equals(date))
                     addedFruits.add(elem);
+
         return addedFruits;
+    }
+
+    private int dateDifference(Date date, Fruit fruit) {
+        return (int)((date.getTime() - fruit.getDate().getTime()) / (24 * 60 * 60 * 1000));
     }
 
     public List<Fruit> getFruitList() {
